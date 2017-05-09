89fc8c6c19b4c013b97d69cae001e7f5692edf5d
Fabio Stivanin <f.stivanin@iconsulting.biz>
2017-05-09 13:16:10 +0200

    fix commit

--- deployer.bat
+++ deployer.bat
@@ -13,7 +13,7 @@ call git push
 cd D:\FormIO\formio-app-formio
 rd /s /q D:\FormIO\formio-app-formio\bower_components\ng-formio-builder
 call bower cache clean
-call bower install
+call bower install ng-formio-builder=https://github.com/aialenti/ngFormBuilder.git#master
 ::	mkdir D:\FormIO\formio-app-formio\bower_components\ng-formio-builder
 ::Copiare tutti i file e le cartelle (tranne bower_components e node_modeules) da ngFormBuilder a app-formio
 ::	ROBOCOPY D:\FormIO\ngFormBuilder D:\FormIO\formio-app-formio\bower_components\ng-formio-builder /E /xd D:\FormIO\ngFormBuilder\bower_components /xd D:\FormIO\ngFormBuilder\node_modules
